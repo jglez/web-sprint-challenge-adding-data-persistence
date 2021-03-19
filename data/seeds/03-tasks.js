c5fe350faa3601178222e961f5159014863bf582
jglez <jglez.cs@gmail.com>
2021-03-19 18:22:03 -0500

    Added tasks seeds

--- data/seeds/03-tasks.js
+++ data/seeds/03-tasks.js
@@ -1,13 +1,19 @@
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('table_name').del()
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        { task_description: 'task 1', task_notes: 'this is a task', task_completed: false, project_id: 1 },
+        { task_description: 'task 2', task_notes: 'this is a task', task_completed: false, project_id: 1 },
+        { task_description: 'task 3', task_notes: 'this is a task', task_completed: false, project_id: 2 },
+        { task_description: 'task 4', task_notes: 'this is a task', task_completed: false, project_id: 2 },
+        { task_description: 'task 5', task_notes: 'this is a task', task_completed: false, project_id: 2 },
+        { task_description: 'task 6', task_notes: 'this is a task', task_completed: false, project_id: 3 },
+        { task_description: 'task 7', task_notes: 'this is a task', task_completed: false, project_id: 3 },
+        { task_description: 'task 8', task_notes: 'this is a task', task_completed: false, project_id: 1 },
+        { task_description: 'task 9', task_notes: 'this is a task', task_completed: false, project_id: 2 }
       ]);
     });
 };
