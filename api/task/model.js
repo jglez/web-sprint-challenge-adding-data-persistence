bbd4e4ef378312e4e1d7e8639f026569cf99ab3d
jglez <jglez.cs@gmail.com>
2021-03-19 17:48:16 -0500

    Added example responses for task-model.js

--- api/task/model.js
+++ api/task/model.js
@@ -1,10 +1,14 @@
 // Tasks Model
 function getAll() {
-  return Promise.resolve('Tasks Get')
+  return Promise.resolve([{
+    "task_id": 1, "task_description": "baz", "task_notes": null, "task_completed": false, "project_name": "bar", "project_description": null
+  }])
 }
 
 function insert() {
-  return Promise.resolve('Tasks Post')
+  return Promise.resolve({
+    "task_id": 1, "task_description": "baz", "task_notes": null, "task_completed": false, "project_id": 1
+  })
 }
 
 module.exports = {
