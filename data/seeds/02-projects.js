c21f04a01d7ebde4ce8fdcf9162e835bf48383df
jglez <jglez.cs@gmail.com>
2021-03-19 18:19:20 -0500

    Added projects seeds

--- data/seeds/02-projects.js
+++ data/seeds/02-projects.js
@@ -1,13 +1,13 @@
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('table_name').del()
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        { project_name: 'project 1', project_description: 'first project', project_completed: false },
+        { project_name: 'project 2', project_description: 'second project', project_completed: false },
+        { project_name: 'project 3', project_description: 'third project', project_completed: false }
       ]);
     });
 };
