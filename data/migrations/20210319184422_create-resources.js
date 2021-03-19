d81e9dd03befc3d59d67e4be7e180bf44d3df8e0
jglez <jglez.cs@gmail.com>
2021-03-19 16:20:21 -0500

    Created resources and tasks table with columns and constraints

--- data/migrations/20210319184422_create-resources.js
+++ data/migrations/20210319184422_create-resources.js
@@ -16,10 +16,22 @@ exports.up = function (knex) {
 
     .createTable('resources', table => {
       table.increments('resource_id')
+      table.string('resource_name').notNullable().unique()
+      table.string('resource_description')
     })
 
     .createTable('tasks', table => {
       table.increments('task_id')
+      table.string('task_description').notNullable()
+      table.string('task_notes')
+      table.boolean('task_completed').defaultTo(false)
+      table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
     })
 
     .createTable('project_resources', table => {
